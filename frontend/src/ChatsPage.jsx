0510c457ccbe9977842714c806c1246a0171523a
Pranay Varma <linkpranayv90@gmail.com>
2023-06-29 19:52:54 +0530

    minor changes

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,14 +1,15 @@
-/* eslint-disable react/prop-types */
-import { PrettyChatWindow } from 'react-chat-engine-pretty'
-const ChatsPage = (props)=>{
-    return (
-    <div style={{height:'100vh'}}>
-        <PrettyChatWindow 
-        projectId='eb026860-8f57-4cd1-ad6b-f6fcfac37114'
+import { PrettyChatWindow } from "react-chat-engine-pretty";
+// import { ChatEngineWrapper, Socket } from "react-chat-engine-pretty";
+const ChatsPage = (props) => {
+  return (
+    <div style={{ height: "100vh" }}>
+      <PrettyChatWindow
+        projectId="eb026860-8f57-4cd1-ad6b-f6fcfac37114"
         username={props.user.username}
         secret={props.user.secret}
-        />
+        // style={{ height: "100%" }}
+      />
     </div>
-    )
-}
+  );
+};
 export default ChatsPage;
